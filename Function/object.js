bed92b86186bc1ebd695012c672f12e2c589532b
maruf-hussain <engmarufhussain@gmail.com>
2023-06-24 00:15:25 +0600

    highlow price phone

--- Function/object.js
+++ Function/object.js
@@ -19,5 +19,45 @@
 
 
 const phones = [
-  
-]
+  {name:'samsung',color:'black',camera:'32 mg',price: 95000},
+  {name:'apple',color:'silver',camera:'48 mg',price: 122000},
+  {name:'vivo',color:'black',camera:'64 mg',price:45000},
+  {name:'samsung',color:'green',camera:'12 mg',price:75000}
+];
+
+function highPricePhones(phones){
+  let hihest = phones[0];
+  for(let i = 0; i < phones.length; i++){
+    const phone = phones[i];
+    if(phone.price > hihest.price){
+     hihest = phone;
+    }
+    // console.log(phone);
+  }
+  return hihest;
+}
+
+
+const mobile = phones;
+const hihPhone = highPricePhones(mobile);
+console.log(hihPhone);
+
+
+
+// // found phones lowest price here
+// function highPricePhones(phones){
+//   let lowest = phones[0];
+//   for(let i = 0; i < phones.length; i++){
+//     const phone = phones[i];
+//     if(phone.price < lowest.price){
+//      lowest = phone;
+//     }
+//     // console.log(phone);
+//   }
+//   return lowest;
+// }
+
+
+// const mobiles = phones;
+// const lowPhone = highPricePhones(mobiles);
+// console.log(lowPhone);
