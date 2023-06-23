97e58f4412aa588c7af561b3228fac8997d14391
maruf-hussain <engmarufhussain@gmail.com>
2023-06-23 23:43:41 +0600

    lsfgjalsjfa

---
+++ Function/object.js
@@ -0,0 +1,23 @@
+// const laptops = [
+//  {Name:'Dell',Procecer:'core i 5 13 gen', Ram:'32 gb',Storage:512, Color:'Black'},
+//  {Name:'Samsung',Procecer:'core i 5 13 gen', Ram:'32 gb',Storage:512, Color:'Black'},
+//  {Name:'Asus',Procecer:'core i 5 13 gen', Ram:'32 gb',Storage:512, Color:'Black'},
+//  {Name:'Hp',Procecer:'core i 5 13 gen', Ram:'32 gb',Storage:512, Color:'Black'},
+//  {Name:'Lenevo',Procecer:'core i 5 13 gen', Ram:'32 gb',Storage:512, Color:'Black'},
+
+// ]
+
+// function lowPriceLaptop(laptops){
+//   for(let i = 0; i < laptops.length; i++){
+//     const laptop = laptops[i];
+//     console.log(laptop);
+//   }
+// }
+
+// const cheapLaptop = lowPriceLaptop(laptops);
+// // console.log(cheapLaptop);
+
+
+const phones = [
+  
+]
